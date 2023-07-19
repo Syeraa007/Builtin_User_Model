51fa908841bd00fe6c895c9c50a86fd8bd62aeae
SYERAA007 <simhasimha4321.com>
2023-07-19 13:53:16 +0530

    first commmit

--- App/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from App.models import *
-
-class ProfileForm(forms.ModelForm):
-    class Meta():
-        model=Profile
-        fields='__all__'
-        label={'pic': 'Picture','address':'Address'}
